7745769eb2057d4e6dc04db0bd4c93efc03bab0e
kvloover <14139943+kvloover@users.noreply.github.com>
2022-05-28 14:38:06 +0200

    config for auto spawn

--- src/room/config/config.ts
+++ src/room/config/config.ts
@@ -1,11 +1,19 @@
 export class config {
+    [key: string]: stageConfig;
+}
+
+export class stageConfig {
+    public order: number = 0;
+    public energy: number = 0;
+    public controller: number = 0;
+    public creeps: number = 0;
     public template: string[] = ["work", "carry", "move"];
     public roles: roleConfig[] = [];
 }
 
 export class roleConfig {
     public priority: number = 9;
-    public role: string | undefined;
+    public role: string = '';
     public count: number = 0;
     public template: string[] | undefined;
 }
