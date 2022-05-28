71e53088115d7f44d21393a6eb048679614242af
kvloover <14139943+kvloover@users.noreply.github.com>
2022-05-28 07:10:03 +0200

    basic tower management

--- src/structures/tower-manager.ts
+++ src/structures/tower-manager.ts
@@ -1,12 +1,40 @@
 import { injectable } from "tsyringe";
 
 import { Manager } from "manager";
+import { TickLogger } from "logger";
 
 injectable()
 export class TowerManager implements Manager {
 
     constructor() { }
 
+    private defend(tower: StructureTower) {
+
+        const closestDamagedStructure = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if (closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
     public run(): void {
+
+        // rewrite
+        Object.keys(Game.rooms)
+            .forEach(key => {
+                const towers = Game.rooms[key].find<StructureTower>(FIND_MY_STRUCTURES,
+                    { filter: (struct) => struct.structureType == STRUCTURE_TOWER });
+                // this.log.Information(`${towers.length} towers found in room ${Game.rooms[key].name}`);
+                if (towers.length > 0) {
+                    towers.forEach(t => this.defend(t));
+                }
+            });
+
     }
 }
