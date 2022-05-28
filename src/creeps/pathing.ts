dd16abf0b052a3470e1382ca99c27dfdf1268efe
kvloover <14139943+kvloover@users.noreply.github.com>
2022-05-28 09:23:05 +0200

    fix path sorting

--- src/creeps/pathing.ts
+++ src/creeps/pathing.ts
@@ -5,19 +5,26 @@ export class Pathing {
 
     public moveToClosest<T extends RoomObject>(creep: Creep, objects: T[]) {
 
-        const closest =
+        const paths =
             objects.map(obj => {
                 return {
                     'loc': obj.pos,
                     // 'path': PathFinder.search(creep.pos, obj.pos)
-                    'path': creep.room.findPath(creep.pos, obj.pos).length
+                    'length': creep.room.findPath(creep.pos, obj.pos).length
                 }
-            })
-                //.sort((a,b) => a.path.cost - b.path.cost)
-                .sort((a, b) => b.path - a.path)
-                .find(i => i != undefined);
+            });
 
-        if (closest && closest.loc && closest.path) {
+        // paths.forEach(i => console.log(`loc: ${i.loc}, length: ${i.length}`));
+
+        const closest = paths
+            //.sort((a,b) => a.path.cost - b.path.cost)
+            .sort((a, b) => a.length - b.length)
+            .find(i => i != undefined);
+
+        //console.log(`closest: ${closest?.loc}, length: ${closest?.length}`)
+
+
+        if (closest && closest.loc && closest.length) {
             creep.moveTo(closest.loc, { visualizePathStyle: { stroke: '#ffaa00' } });
         }
     }
