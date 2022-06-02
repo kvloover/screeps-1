420f9d65d2ff2755776aca2ed9d2959a7f057e5a
kvloover <14139943+kvloover@users.noreply.github.com>
2022-06-02 19:59:49 +0200

    format

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import "reflect-metadata";
-import { ErrorMapper } from "utils/ErrorMapper";
 import { container } from "tsyringe";
 
-import { GameWorld } from "game-world";
+import { ErrorMapper } from "utils/ErrorMapper";
 
+import { GameWorld } from "game-world";
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-
   container.resolve(GameWorld)
     .run();
-
 });
