682ab4b0eb9c3f1d12e63bba39e97985b6bc6293
Kevin Van Looveren <14139943+kvloover@users.noreply.github.com>
2022-06-03 13:46:03 +0000

    no need to specify identical ctor

--- src/tasks/task.ts
+++ src/tasks/task.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid';
 
-export abstract class Task {
+export class Task {
     id: string = uuid();
     constructor(
         public prio: number,
@@ -11,76 +11,12 @@ export abstract class Task {
     ) { }
 }
 
-export class AttackTask extends Task {
-    constructor(
-        prio: number,
-        amount?: number,
-        requester?: Id<_HasId>,
-        executer?: Id<_HasId>,
-        pos?: _HasRoomPosition
-    ) { super(prio, amount, requester, executer, pos) }
-}
-export class BuildTask extends Task {
-    constructor(
-        prio: number,
-        amount?: number,
-        requester?: Id<_HasId>,
-        executer?: Id<_HasId>,
-        pos?: _HasRoomPosition
-    ) { super(prio, amount, requester, executer, pos) }
-}
-export class ClaimTask extends Task {
-    constructor(
-        prio: number,
-        amount?: number,
-        requester?: Id<_HasId>,
-        executer?: Id<_HasId>,
-        pos?: _HasRoomPosition
-    ) { super(prio, amount, requester, executer, pos) }
-}
-export class HarvestTask extends Task {
-    constructor(
-        prio: number,
-        amount?: number,
-        requester?: Id<_HasId>,
-        executer?: Id<_HasId>,
-        pos?: _HasRoomPosition
-    ) { super(prio, amount, requester, executer, pos) }
-}
+export class AttackTask extends Task {}
+export class BuildTask extends Task {}
+export class ClaimTask extends Task {}
+export class HarvestTask extends Task {}
 // RangedAttack, RangedHeal, RangedMassAttack, Pull,
-export class RepairTasl extends Task {
-    constructor(
-        prio: number,
-        amount?: number,
-        requester?: Id<_HasId>,
-        executer?: Id<_HasId>,
-        pos?: _HasRoomPosition
-    ) { super(prio, amount, requester, executer, pos) }
-}
-export class TransferTask extends Task {
-    constructor(
-        prio: number,
-        amount?: number,
-        requester?: Id<_HasId>,
-        executer?: Id<_HasId>,
-        pos?: _HasRoomPosition
-    ) { super(prio, amount, requester, executer, pos) }
-}
-export class ContainerTransferTask extends Task {
-    constructor(
-        prio: number,
-        amount?: number,
-        requester?: Id<_HasId>,
-        executer?: Id<_HasId>,
-        pos?: _HasRoomPosition
-    ) { super(prio, amount, requester, executer, pos) }
-}
-export class WithdrawTask extends Task {
-    constructor(
-        prio: number,
-        amount?: number,
-        requester?: Id<_HasId>,
-        executer?: Id<_HasId>,
-        pos?: _HasRoomPosition
-    ) { super(prio, amount, requester, executer, pos) }
-}
+export class RepairTasl extends Task {}
+export class TransferTask extends Task {}
+export class ContainerTransferTask extends Task {}
+export class WithdrawTask extends Task {}
