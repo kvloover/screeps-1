d2242ea9d0cf9e12217fb9c55d2c8dd0e1193f4d
kvloover <14139943+kvloover@users.noreply.github.com>
2022-06-05 10:35:39 +0200

    add attack/remote commands

--- src/utils/console.ts
+++ src/utils/console.ts
@@ -2,6 +2,8 @@ export class ExConsole {
     static init() {
         global.debug = this.debug;
         global.stopDebug = this.stopDebug;
+        global.remote = this.remote;
+        global.attack = this.attack;
     }
 
     static debug(room: string): string {
@@ -19,6 +21,22 @@ export class ExConsole {
         }
         return `Room not known: ${room}`
     }
+
+    static remote(room: string, value: string | undefined): string {
+        if (Memory.rooms.hasOwnProperty(room)) {
+            Memory.rooms[room].remote = value;
+            return `Remote set for ${room}.`;
+        }
+        return `Room not known: ${room}`
+    }
+
+    static attack(room: string, value: string | undefined): string {
+        if (Memory.rooms.hasOwnProperty(room)) {
+            Memory.rooms[room].attack = value;
+            return `Attack set for ${room}.`;
+        }
+        return `Room not known: ${room}`
+    }
 }
 
 declare global {
@@ -26,6 +44,8 @@ declare global {
         interface Global {
             debug: (room: string) => string;
             stopDebug: (room: string) => string;
+            remote: (room: string, value: string | undefined) => string;
+            attack: (room: string, value: string | undefined) => string;
         }
     }
 }
