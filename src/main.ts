55909c59a0bf9bb0101eca187bd1e3f74a0aa74c
kvloover <14139943+kvloover@users.noreply.github.com>
2022-06-05 11:21:26 +0200

    enable memory hack

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,17 @@ import "reflect-metadata";
 import { container } from "tsyringe";
 
 import { ErrorMapper } from "utils/ErrorMapper";
-import { wrapWithMemoryHack } from "utils/memory-hack";
+import { wrapMemory } from "utils/memory-hack";
 import { ExConsole } from "utils/console";
 
 import { GameWorld } from "game-world";
 
 export const loop = ErrorMapper.wrapLoop(
-  //wrapWithMemoryHack( // Disabled for now to allow console memory editing
+  wrapMemory(
     () => {
-
       ExConsole.init();
-
       container.resolve(GameWorld)
         .run();
     }
-  //)
+  )
 );
--- src/utils/memory-hack.ts
+++ src/utils/memory-hack.ts
@@ -1,8 +1,21 @@
 declare const global: { Memory?: Memory };
 
+// there are two ways of saving Memory with different advantages and disadvantages
+// 1. RawMemory.set(JSON.stringify(Memory));
+// + ability to use custom serialization method
+// - you have to pay for serialization
+// - unable to edit Memory via Memory watcher or console
+// 2. RawMemory._parsed = Memory;
+// - undocumented functionality, could get removed at any time
+// + the server will take care of serialization, it doesn't cost any CPU on your site
+// + maintain full functionality including Memory watcher and console
+
+// this implementation uses the official way of saving Memory
+
 // https://github.com/Jomik/screeps-ai/blob/7b8ca87028df257471f43dc9b13b9ee362bf7f30/packages/bot/src/utils/memory-hack.ts
 // Adapted from https://github.com/screepers/screeps-snippets/blob/8b557a3fcb82cb734fca155b07d5a48622f9da60/src/misc/JavaScript/Memory%20Cache.js
-export const wrapWithMemoryHack = (fn: () => void) => {
+
+export const wrapMemory = (fn: () => void) => {
   const memory = Memory;
 
   return () => {
@@ -11,17 +24,6 @@ export const wrapWithMemoryHack = (fn: () => void) => {
 
     fn();
 
-    // there are two ways of saving Memory with different advantages and disadvantages
-    // 1. RawMemory.set(JSON.stringify(Memory));
-    // + ability to use custom serialization method
-    // - you have to pay for serialization
-    // - unable to edit Memory via Memory watcher or console
-    // 2. RawMemory._parsed = Memory;
-    // - undocumented functionality, could get removed at any time
-    // + the server will take care of serialization, it doesn't cost any CPU on your site
-    // + maintain full functionality including Memory watcher and console
-
-    // this implementation uses the official way of saving Memory
     RawMemory.set(JSON.stringify(Memory));
   };
 };
