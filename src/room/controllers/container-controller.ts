f4db7bac5ce4df4a1d1c2c3ea77ede1ca85f475f
kvloover <14139943+kvloover@users.noreply.github.com>
2022-06-06 21:28:32 +0200

    drops prio 1, mid stream prio 2

--- src/room/controllers/container-controller.ts
+++ src/room/controllers/container-controller.ts
@@ -32,7 +32,7 @@ export class ContainerController implements Controller {
                 const current = this.transferRepo.getForRequester(i.id);
                 const amount = current.reduce((p, c) => p + (c.amount ?? 0), 0);
                 if (amount < free) {
-                    this.transferRepo.add(new MidstreamTask(struct.room.name, 1, free - amount, i.id, undefined, i.pos));
+                    this.transferRepo.add(new MidstreamTask(struct.room.name, 2, free - amount, i.id, undefined, i.pos));
                     this.log.debug(room, `${i.pos}: added container midstream task`);
                 }
             }
@@ -42,7 +42,7 @@ export class ContainerController implements Controller {
                 const current = this.providerRepo.getForRequester(i.id);
                 const amount = current.reduce((p, c) => p + (c.amount ?? 0), 0);
                 if (amount < stored) {
-                    this.providerRepo.add(new ProviderTask(struct.room.name, 1, stored - amount, i.id, undefined, i.pos));
+                    this.providerRepo.add(new ProviderTask(struct.room.name, 2, stored - amount, i.id, undefined, i.pos));
                     this.log.debug(room, `${i.pos}: added container provider task`);
                 }
             }
