25b4ab881897276048ded8aa9066febfead02494
kvloover <14139943+kvloover@users.noreply.github.com>
2022-06-11 19:51:12 +0200

    bugfix builder, using targetId

--- src/creeps/roles/builder/builder-service.ts
+++ src/creeps/roles/builder/builder-service.ts
@@ -7,6 +7,7 @@ import { BuilderRole } from "./builder-role";
 import { SupplyTaskRepo } from "repos/supply-task-repo";
 import profiler from "screeps-profiler";
 import { HarvestAction } from "../harvester/harvest-action";
+import { CreepState } from "utils/creep-state";
 
 @injectable()
 export class BuilderSourceRole extends BuilderRole {
@@ -22,12 +23,18 @@ export class BuilderSourceRole extends BuilderRole {
     }
 
     protected consume(creep: Creep): void {
+        this.log.debug(creep.room, `${creep.name} harvesting`);
         this.harvesting.Action(creep);
     }
 
     public run(creep: Creep): void {
         this.log.debug(creep.room, `Running builder source`);
         super.run(creep);
+    }
+
+    protected override setState(creep: Creep, state: CreepState): void {
+        super.setState(creep, state);
+        creep.memory.targetId = undefined; // used by both harvesting and building
     }
 }
 
