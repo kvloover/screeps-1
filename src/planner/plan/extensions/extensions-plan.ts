3d1ea8994dd3137e391d588022d264b3c66dea4f
kvloover <14139943+kvloover@users.noreply.github.com>
2022-07-13 22:00:01 +0200

    bugfix but use centers for cleaner plan

--- src/planner/plan/extensions/extensions-plan.ts
+++ src/planner/plan/extensions/extensions-plan.ts
@@ -28,12 +28,13 @@ export class ExtensionsPlan implements IPlan {
 
         // get initial seeds as outer side of anchor stamp
         const seeds = outerPerimeter(roomName, terrain, poi.anchor?.map(i => { return { x: i.x, y: i.y } }) || [], false, CUTOFF_WALKABLE);
+        // const seeds: Point[] = poi.anchor?.map(i => { return { x: i.x, y: i.y } }) || [];
         this.log.debug(roomName, `ExtensionPlan: anchor seeds: ${seeds.map(x => `${x.x},${x.y}`).join(';')}`);
 
         const sorted = data.plans.sort((a, b) => a.priority - b.priority);
         let extensions = 0;
         while (extensions < 50) {
-            const dt = distanceTransform(roomName, terrain, distanceType.Manhattan, false, 220, { x1: 2, y1: 2, x2: 47, y2: 47 }); // do not include roads
+            const dt = distanceTransform(roomName, terrain, distanceType.Manhattan, false, CUTOFF_WALKABLE, { x1: 2, y1: 2, x2: 47, y2: 47 }); // do not include roads
             let center: Point | undefined = undefined;
             let index = 0;
             while (!center && index < sorted.length) {
@@ -41,14 +42,15 @@ export class ExtensionsPlan implements IPlan {
                 const plan = sorted[index];
                 index++;
 
-                const size = Math.max(Math.ceil((plan.size.x + 1) / 2), Math.ceil((plan.size.y + 1) / 2));
+                const size = Math.max(Math.ceil(plan.size.x / 2), Math.ceil(plan.size.y / 2));
                 center = findPointFor(roomName, dt, seeds, n => n >= size, false, false, CUTOFF_WALKABLE);
                 if (!center) { continue; }
 
                 // update seeds for next plan
-                const borderStamps = this.borderStamp(center, size);
-                this.log.debug(roomName, `ExtensionPlan: border seeds for center ${center.x},${center.y}: ${borderStamps.map(x => `${x.x},${x.y}`).join(';')}`);
-                borderStamps.forEach(p => seeds.push(p));
+                // const borderStamps = this.borderStamp(center, size);
+                // this.log.debug(roomName, `ExtensionPlan: border seeds for center ${center.x},${center.y} size ${size}: ${borderStamps.map(x => `${x.x},${x.y}`).join(';')}`);
+                // borderStamps.forEach(p => seeds.push(p));
+                seeds.push(center);
 
                 // add buildings in plan to matrix
                 const structures: StructurePlan[] = [];
@@ -85,13 +87,16 @@ export class ExtensionsPlan implements IPlan {
     private borderStamp(center: Point, size: number): Point[] {
         // consider currently using a diamond stamp
         const points: Point[] = [];
-        if (size = 1) return [center];
+        if (size == 1) return [center];
 
-        for (let i = 0; i < size; i++) {
+        points.push({ x: center.x, y: center.y + size });
+        points.push({ x: center.x, y: center.y - size });
+        points.push({ x: center.x - size, y: center.y });
+        points.push({ x: center.x + size, y: center.y });
+
+        for (let i = 1; i < size; i++) {
             points.push({ x: center.x + i, y: center.y + size - i });
             points.push({ x: center.x + i, y: center.y - size + i });
-            points.push({ x: center.x - i, y: center.y + size - i });
-            points.push({ x: center.x - i, y: center.y - size + i });
         }
 
         return points;
