9c60e6ea3248239848565d554478e578fefccca7
kvloover <14139943+kvloover@users.noreply.github.com>
2022-07-30 15:38:15 +0200

    add positions to source memory

--- src/controllers/source-controller.ts
+++ src/controllers/source-controller.ts
@@ -9,6 +9,7 @@ import { initObjectMemory } from "structures/memory/structure-memory";
 import { SOURCE } from "utils/custom-types";
 
 import profiler from "screeps-profiler";
+import { posix } from "path";
 
 @injectable()
 export class SourceController implements Controller {
@@ -38,14 +39,32 @@ export class SourceController implements Controller {
     // Set flags on sources and store names in room memory
     private initializeRoom(room: Room): void {
 
+        const terrain = Game.map.getRoomTerrain(room.name);
         initObjectMemory(room.memory, SOURCE);
 
         // Add task for each source to be mined | TODO configurable ammount of harvesters
         room.find(FIND_SOURCES).forEach(src => {
             // 3000 energy per tick, reset every 300 ticks
             // this.log.debug(src.room, `${src.pos}: added harvest task`);
-            room.memory.objects?.source?.push({ id: src.id, pos: src.pos, type: SOURCE });
+            const positions = this.nearestPositions(src.pos)
+                .map<number>(pos => terrain.get(pos.x, pos.y) === TERRAIN_MASK_WALL ? 0 : 1)
+                .reduce((sum, val) => sum + val, 0);
+            const sourceMem: SourceMemory = { id: src.id, pos: src.pos, type: SOURCE, positions: positions };
+            room.memory.objects?.source?.push(sourceMem);
         })
+    }
+
+    private nearestPositions(pos: RoomPosition): RoomPosition[] {
+        const positions = [];
+        for (let x = pos.x - 1; x <= pos.x + 1; x++) {
+            for (let y = pos.y - 1; y <= pos.y + 1; y++) {
+                if (x < 0 || x >= 50 ||
+                    y < 0 || y >= 50) continue
+                if (x == pos.x && y == pos.y) continue;
+                positions.push(new RoomPosition(x, y, pos.roomName));
+            }
+        }
+        return positions;
     }
 }
 
