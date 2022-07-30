db01b6f8e5516232499a512500ea4ed422a4f81e
kvloover <14139943+kvloover@users.noreply.github.com>
2022-07-30 19:32:27 +0200

    clear memory on respawn

--- src/init-respawn.ts
+++ src/init-respawn.ts
@@ -4,16 +4,23 @@ import { SpawnManager } from "structures";
 export function initializeOnRespawn(): boolean {
 
     let initialized = false;
-    for (let room of Object.values(Game.rooms)) {
-        console.log(`Initializing room ${room.name}`);
-        SpawnManager.init(room);
-        initialized = true;
-    }
+
+    global.refs = undefined;
+    global.repair = undefined;
+    Memory.persistency = {} as any;
 
     for (const name in Memory.rooms) {
         if (!(name in Game.rooms)) {
             delete Memory.rooms[name];
+        } else {
+            Memory.rooms[name] = {} as any;
         }
+    }
+
+    for (let room of Object.values(Game.rooms)) {
+        console.log(`Initializing room ${room.name}`);
+        SpawnManager.init(room);
+        initialized = true;
     }
 
     return initialized;
